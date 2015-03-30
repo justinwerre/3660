cc903e9d28eb043d36b6b4da576297103592ea6d
justinwerre <werrejustin@gmail.com>
2015-03-30 11:15:26 -0600

    worked on purchase Games

--- public/php/purchaseGames.php
+++ public/php/purchaseGames.php
@@ -2,35 +2,47 @@
   include "databaseConnect.php"; 
   session_start();
   $customer_id = $_SESSION['ID'];
+  $success = true;
+  $con->begin_transaction();
 
-  // select the games the user wishes to buy
-  $query = "SELECT SHOPPINGCART.serial_number, VIDEOGAMES.price
-          FROM SHOPPINGCART
-          INNER JOIN CUSTOMERS ON CUSTOMERS.cID = SHOPPINGCART.cID
-          INNER JOIN VIDEOGAMES ON VIDEOGAMES.serial_number = SHOPPINGCART.serial_number
-          WHERE SHOPPINGCART.cID = {$customer_id};";
-  $cart = $con->query($query);
-  var_dump($con);
-  var_dump($cart);
-  $today = date("Y-m-d");
+    // select the games the user wishes to buy
+    $query = "SELECT SHOPPINGCART.serial_number, VIDEOGAMES.price
+            FROM SHOPPINGCART
+            INNER JOIN CUSTOMERS ON CUSTOMERS.cID = SHOPPINGCART.cID
+            INNER JOIN VIDEOGAMES ON VIDEOGAMES.serial_number = SHOPPINGCART.serial_number
+            WHERE SHOPPINGCART.cID = {$customer_id};";
+    $cart = $con->query($query);
+    if( false == $cart)
+      $success = false;
+    $today = date("Y-m-d");
 
-  // create a purchase record
-  $query = "INSERT INTO PURCHASES(cID, pDate)
-            VALUES ({$customer_id}, '{$today}');";
-  $t = $con->query($query);
-  $purchaseId = $con->insert_id;
+    // create a purchase record
+    $query = "INSERT INTO PURCHASES(cID, pDate)
+              VALUES ({$customer_id}, '{$today}');";
+    $t = $con->query($query);
+    if( false == $t )
+      $success = false;
+    $purchaseId = $con->insert_id;
 
-  // add the games to the purchase record
-  while($row = $cart->fetch_assoc()){
-    $query = "INSERT INTO ORDERED(pID, serial_number, price)
-              VALUES ({$purchaseId}, {$row['serial_number']}, {$row['price']})";
-    $con->query($query);
+    // add the games to the purchase record
+    while($row = $cart->fetch_assoc()){
+      $query = "INSERT INTO ORDERED(pID, serial_number, price)
+                VALUES ({$purchaseId}, {$row['serial_number']}, {$row['price']})";
+      if ( false == $con->query($query) )
+        $success = false;
+    }
+
+    // delete the shopping cart
+    $query = "DELETE FROM SHOPPINGCART
+              WHERE cID = {$customer_id};";
+    if( flase == $con->query($query) )
+      $success = false;
+
+  if( $success ){
+    $con->commit();
+  }else{
+    $con->rollback();
   }
-
-  // delete the shopping cart
-  $query = "DELETE FROM SHOPPINGCART
-            WHERE cID = {$customer_id};";
-  $con->query($query);
 
   $con->close();
 ?>
@@ -45,11 +57,11 @@
   </head>
   <body>
     <div class="container">
-      <h1 class="header">Shopping Cart</h1>
-      <table class="table table-bordered table-striped">
-      </table>
-			<a class='btn btn-success pull-right' href='checkout.php'>Checkout</a>
-			<a class='btn btn-primary' href="listGames.php">Continue Shopping</a>
+        <?php if ($success): ?>
+          <h1 class="header">Purchase Successful</h1>
+          <a class='btn btn-success pull-right' href=''>Library</a>
+          <a class='btn btn-primary' href="listGames.php">Continue Shopping</a>
+        <?php endif ?>
     </div>
   </body>
 </html>
